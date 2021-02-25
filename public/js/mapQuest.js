b3e381e8d7c521e21ed5d9fb83e293ce7ce989a1
Cuien Wu <cuw002@ucsd.edu>
2021-02-25 13:17:44 -0800

    lab 8 changes

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,17 @@
 function initMap() {
 	// add your code here
+	/*L.mapquest.key = 'QyR5vVHIpk9hzkUrSI3bzcm6dOGXpquZ';*/
+
+	L.mapquest.key = 'QyR5vVHIpk9hzkUrSI3bzcm6dOGXpquZ';
+
+// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.87497819956424, -117.23773557792556],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	  /* map won't show up with zoomControl */
+	  /*zoomControl: false*/
+	});
+
+	L.marker([32.87497819956424, -117.23773557792556]).addTo(map);
 }
